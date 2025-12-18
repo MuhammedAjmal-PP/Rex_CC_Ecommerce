4b462fdc3693b643b8b0bac451ba41991eca12ab
muha_ajmal <muhammedajmalbinali@gmail.com>
2025-12-18 11:03:53 +0530

    feat(forms): add regex validation for first and last name

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from allauth.account.forms import SignupForm
 from phonenumber_field.formfields import PhoneNumberField
+from django.core.validators import RegexValidator
 
+
+alphabet_only=RegexValidator(
+    regex=r'^[a-zA-Z]+$',
+    message="This field should only contain letters",
+    code='invalid_alphabet'
+)
 
 class CustomSignupForm(SignupForm):
     """Custom signup form for django-allauth with additional fields."""
@@ -9,6 +16,7 @@ class CustomSignupForm(SignupForm):
     first_name = forms.CharField(
         max_length=100,
         required=True,
+        validators=[alphabet_only],
         widget=forms.TextInput(
             attrs={
                 "placeholder": "First Name",
@@ -22,6 +30,7 @@ class CustomSignupForm(SignupForm):
     last_name = forms.CharField(
         max_length=100,
         required=False,
+        validators=[alphabet_only],
         widget=forms.TextInput(
             attrs={
                 "placeholder": "Last Name",
@@ -49,6 +58,7 @@ class CustomSignupForm(SignupForm):
         user = super(CustomSignupForm, self).save(request)
         user.first_name = self.cleaned_data["first_name"]
         user.last_name = self.cleaned_data["last_name"]
-        user.phone = self.cleaned_data.get("phone", "")
+        user.phone_number = self.cleaned_data["phone_number"]
         user.save()
         return user
+    
