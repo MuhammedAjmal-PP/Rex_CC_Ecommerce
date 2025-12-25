bd0ac18e9add0cbb0fc5a59030bcd29a15ef5071
muha_ajmal <muhammedajmalbinali@gmail.com>
2025-12-25 16:54:57 +0530

    feat: add Category model to catalog app

--- catalog/models.py
+++ catalog/models.py
@@ -38,3 +38,30 @@ class Brand(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Category(models.Model):
+
+    name = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+
+        if self.pk:
+            old_name = Category.objects.values_list("name", flat=True).get(pk=self.pk)
+            if old_name != self.name:
+                self.slug = slugify(self.name)
+        else:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
