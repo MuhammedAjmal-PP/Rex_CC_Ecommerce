f50a19ad7f83cabf643cb69de781c5ecee4a5236
muha_ajmal <muhammedajmalbinali@gmail.com>
2025-12-29 17:16:33 +0530

    chore: removed urls

--- catalog/urls/user_urls.py
+++ catalog/urls/user_urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from catalog.views.user.product import product_list, product_detail
+from catalog.views.user.product import product_list
 
 urlpatterns = [
     path("products/", product_list, name="product_list"),
-    path("product/<slug:slug>/", product_detail, name="product_detail"),
 ]
